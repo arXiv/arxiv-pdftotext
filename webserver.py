2f5c689f312a7a23cd3fa6f257ab92f1634d04cc
De Nolan <t4zhai@edu.uwaterloo.ca>
2024-10-17 23:36:24 -0400

    add version endpoint; fix yaml to build with --mount option

--- webserver.py
+++ webserver.py
@@ -9,7 +9,7 @@ import sys
 from subprocess import PIPE, Popen
 from tempfile import TemporaryDirectory
 
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, jsonify
 
 # map mode parameters to programs
 PARAM2PROGRAM_DEFAULTS = {
@@ -79,6 +79,13 @@ def handle_file():
 
         return send_file(file_path_out)
 
+@app.route('/version')
+def version():
+    build_version = os.getenv('BUILD_VERSION', 'unknown')
+
+    return jsonify({
+        'version': build_version
+    })
 
 if __name__ == "__main__":
     host = "0.0.0.0"
