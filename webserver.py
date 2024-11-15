514b1408f09cc70170caf8915c50521070ff2212
Norbert Preining <norbert@arxiv.org>
2024-11-15 09:44:47 +0900

    Rewrite using fastapi

--- webserver.py
+++ webserver.py
@@ -6,84 +6,76 @@ import os
 import shutil
 import socket
 import sys
+import tempfile
 from subprocess import PIPE, Popen
-from tempfile import TemporaryDirectory
 
-from flask import Flask, request, send_file
+from fastapi import FastAPI, File, HTTPException, UploadFile
+from fastapi.responses import FileResponse
+from starlette.background import BackgroundTask
 
-# map mode parameters to programs
 PARAM2PROGRAM_DEFAULTS = {
     "pdftotext": "pdftotext",
     "pdf2txt": "pdf2txt.py",
 }
 PARAM2PROGRAM_FOUND = {}
-app = Flask(__name__)
+
+app = FastAPI()
 
 for k, v in PARAM2PROGRAM_DEFAULTS.items():
     if shutil.which(v) is not None:
         PARAM2PROGRAM_FOUND[k] = v
 
 
-@app.route("/", methods=["GET"])
+@app.get("/")
 def healthcheck() -> str:
     """Health check endpoint."""
     return "<h1>All good!</h1>"
 
 
-@app.route("/", methods=["POST"])
-def handle_file():
+@app.post("/")
+def handle_file(file: UploadFile = File(...), mode: str = "pdftotext", params: str = ""):
     """Entry point for API call to convert pdf to text."""
-    with TemporaryDirectory() as temp_dir:
-        if "file" in request.files:
-            if len(request.files) != 1:
-                return f"You must upload one PDF file for processing (len(request.files)={len(request.files)})", 400
+    temp_dir = tempfile.mkdtemp()
+    file_path_in = os.path.join(temp_dir, file.filename)
+    if not file_path_in.lower().endswith(".pdf"):
+        return f"Only .pdf files are allowed (file_path_in={file_path_in})", 400
+    try:
+        with open(file_path_in, "wb") as f:
+            shutil.copyfileobj(file.file, f)
+    except Exception:
+        raise HTTPException(status_code=500, detail="Something went wrong")
+    finally:
+        file.file.close()
 
-            files_dict = request.files.to_dict()
+    file_path_out = file_path_in + ".txt"
+    if mode not in PARAM2PROGRAM_FOUND.keys():
+        return f"Program for mode {mode} not found", 400
+    cmd = PARAM2PROGRAM_FOUND[mode]
 
-            for key in files_dict:
-                file = files_dict[key]
-                file_path_in = os.path.join(temp_dir, file.filename)
+    logging.debug("mode=%s file_path_in=%s file_path_out=%s params=%s", mode, file_path_in, file_path_out, params)
+    args = []
+    if params:
+        args = params.split()
+    if mode == "pdf2txt":
+        args.extend(["--outfile", file_path_out, file_path_in])
+    elif mode == "pdftotext":
+        args.extend([file_path_in, file_path_out])
+    else:
+        return f"Invalid mode: {mode}", 400
 
-                if not file_path_in.lower().endswith(".pdf"):
-                    return f"Only .pdf files are allowed (file_path_in={file_path_in})", 400
+    logging.debug(f"Running {[cmd, *args]}")
+    p = Popen([cmd, *args], stdout=PIPE, stderr=PIPE)
+    logging.debug("Starting conversion process")
+    out, err = p.communicate()
+    logging.debug(f"stdout={out}, stderr={err}")
+    print("Conversion process finished")
 
-                file.save(file_path_in)
-        else:  # PDF provided in binary (application/octet-stream), not as a file
-            file_path_in = os.path.join(temp_dir, "unnamed.pdf")
-            data = request.stream.read()
-            with open(file_path_in, "wb") as f:
-                f.write(data)
-            sys.stdout.flush()
+    if p.returncode == 0:
+        return FileResponse(file_path_out, background=BackgroundTask(shutil.rmtree, temp_dir))
+    else:
+        shutil.rmtree(temp_dir)
+        return f"Failed to execute '{mode}' process:\n\n" + err.decode("utf-8"), 500
 
-        file_path_out = file_path_in + ".txt"
-        mode = request.values.get("mode")
-        if mode is None:
-            mode = "pdftotext"
-        if mode not in PARAM2PROGRAM_FOUND.keys():
-            return f"Program for mode {mode} not found", 400
-        cmd = [PARAM2PROGRAM_FOUND[mode]]
-
-        params = request.values.get("params")
-
-        logging.debug("mode=%s file_path_in=%s file_path_out=%s params=%s", mode, file_path_in, file_path_out, params)
-        if params:
-            cmd.extend(params.split())
-        if mode == "pdf2txt":
-            cmd.extend(["--outfile", file_path_out, file_path_in])
-        elif mode == "pdftotext":
-            cmd.extend([file_path_in, file_path_out])
-        else:
-            return f"Invalid mode: {mode}", 400
-
-        p = Popen(cmd, stdout=PIPE, stderr=PIPE)
-        out, err = p.communicate()
-
-        if p.returncode == 0:
-            return send_file(file_path_out)
-        else:
-            return f"Failed to execute '{mode}' process:\n\n" + err.decode("utf-8"), 500
-
-        return send_file(file_path_out)
 
 if __name__ == "__main__":
     host = "0.0.0.0"
